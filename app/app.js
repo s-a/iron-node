32604dc67f2c128f1055f11ace86256aed820aba
s-a <stephan.ahlf@googlemail.com>
2015-07-19 09:37:23 +0200

    added handler for uncaught process exception. fixes #6

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+var remote = require('remote');
+var commandLineArguments = null;
 
 var boot = function() {
   var args = JSON.parse(process.env.commandLineArguments);
@@ -8,9 +10,15 @@ var boot = function() {
     process.argv.push(arg);
   }
 
+  commandLineArguments = args;
   require(args[2]);
 }
 
 window.onload = function() {
   window.setTimeout(boot, 1000);
 }
+
+process.on('uncaughtException', function(err) {
+	alert("uncaughtException:\n" + commandLineArguments[2] + "\n" + err);
+	remote.getCurrentWindow().close();
+});
