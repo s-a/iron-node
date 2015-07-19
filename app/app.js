2fedba4b13f169befd349adf9afe9cfb975e4b12
s-a <stephan.ahlf@googlemail.com>
2015-07-19 09:56:26 +0200

    Allow relative paths of start script. fixes #5

--- app/app.js
+++ app/app.js
@@ -1,24 +1,56 @@
+var fs = require('fs');
+var path = require('path');
 var remote = require('remote');
 var commandLineArguments = null;
 
-var boot = function() {
-  var args = JSON.parse(process.env.commandLineArguments);
-  console.log(args);
-
-  for (var i = 2; i < args.length; i++) {
-    var arg = args[i];
-    process.argv.push(arg);
-  }
-
-  commandLineArguments = args;
-  require(args[2]);
+var error = function(msg) {
+	alert(msg);
+	remote.getCurrentWindow().close();
 }
+
+var prepareStartScriptParameter = function(filename) {
+	var result = filename;
+
+	if (!path.isAbsolute(filename)){
+		result = path.resolve(process.cwd(), filename);
+	}
+
+	if (!fs.existsSync(filename)){
+		error(result + " not found.");
+	}
+
+	return result;
+};
+
+var boot = function() {
+	var args = JSON.parse(process.env.commandLineArguments);
+
+	for (var i = 2; i < args.length; i++) {
+		var arg = args[i];
+		process.argv.push(arg);
+	}
+
+	if (args[2]){
+		args[2] = prepareStartScriptParameter(args[2]);
+	}
+
+	commandLineArguments = args;
+
+	if (args[2]){
+		require(args[2]);
+	}
+}
+
+process.on('uncaughtException', function(err) {
+	var msg = ["Uncaught Exception:"];
+	if (commandLineArguments[2]){
+		msg.push(commandLineArguments[2]);
+	}
+	msg.push(err);
+	error(msg.join("\n"))
+});
 
 window.onload = function() {
   window.setTimeout(boot, 1000);
 }
 
-process.on('uncaughtException', function(err) {
-	alert("uncaughtException:\n" + commandLineArguments[2] + "\n" + err);
-	remote.getCurrentWindow().close();
-});
