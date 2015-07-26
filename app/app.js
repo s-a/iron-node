263b0f3df797358db25f350d5ff455a5689a8c5b
s-a <stephan.ahlf@googlemail.com>
2015-07-26 22:29:31 +0200

    added package.json information ti info window

--- app/app.js
+++ app/app.js
@@ -47,7 +47,48 @@ var prepareStartScriptParameter = function(filename) {
 	return result;
 };
 
+var initializePackageScripts = function(json) {
+	var packageMeta = json;
+	var scripts = [];
+	if (packageMeta.scripts){
+		for(var script in packageMeta.scripts){
+			if (packageMeta.scripts.hasOwnProperty(script)){
+				scripts.push('<a class="menu-item" href="#"><span class="octicon octicon-terminal"></span>' + script + ' : ' + packageMeta.scripts[script] + '</a>');
+			}
+		}
+	}
+	document.getElementById("project-terminal").innerHTML = scripts.join("");
+}
+
+var initializePackageInfo = function(rootDirectory){
+	var p = path.join(rootDirectory, "package.json");
+	fs.exists(p, function(exists){
+		if (exists){
+			fs.readFile(p, function(err, data){
+				if (err){
+					console.error(err);
+				} else {
+					try{
+						document.getElementById("project-package").innerHTML = '<a class="menu-item" href="#"><span class="octicon octicon-package"></span><span>' + p + '</span></a>';
+						var meta = JSON.parse(data.toString());
+						initializePackageScripts(meta);
+						if (meta.repository && meta.repository.url){
+							document.getElementById("project-repo-url").innerHTML = '<a class="menu-item" href="#" onclick="window.opener(\'' + meta.repository.url + '\')"><span class="octicon octicon-repo"></span>Repository</a>';
+						}
+						if (meta.bugs && meta.bugs.url){
+							document.getElementById("project-bugs-url").innerHTML = '<a class="menu-item" href="#" onclick="window.opener(\'' + meta.bugs.url + '\')"><span class="octicon octicon-bug"></span>Issues</a>';
+						}
+					} catch (e){
+						console.error("Error in", p, e);
+					}
+				}
+			});
+		}
+	});
+}
+
 var initializeInfoWindow = function(rootDirectory) {
+	initializePackageInfo(rootDirectory);
 	var filename = path.join(rootDirectory, "DEBUG.md");
 	var loadMarkdownFile = function(fn) {
 		fs.readFile(fn, function(err, data){
