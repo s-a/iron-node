87d2b09277c2cf4d01b63eef377529c4a10c037e
s-a <stephan.ahlf@googlemail.com>
2015-08-07 14:37:36 +0200

    added config.js

---
+++ app/config.js
@@ -0,0 +1,40 @@
+var fs = require("fs");
+var path = require("path");
+var app; 
+
+
+try{
+	app = require("app");
+} catch(e){
+	app = require("remote").require("app");
+}
+
+var Config = function(argv) {
+	var result = null;
+	var dir = "";
+
+	if (argv[2]){
+		dir = argv[2];
+	}
+	var configFilename = path.join(path.resolve(path.dirname(dir)), ".iron-node.js");
+	if (!fs.existsSync(configFilename)){
+		configFilename = path.join(process.cwd(), path.resolve(path.dirname(dir)), ".iron-node.js");
+	}
+	if (!fs.existsSync(configFilename)){
+		configFilename = path.join( app.getPath("appData"), "iron-node", ".iron-node.js");
+	}
+	if (fs.existsSync(configFilename)){
+		try{
+			result = {
+				filename : configFilename,
+				settings : require(configFilename)
+			};
+		} catch(e){
+			console.error(e);
+		}
+	}
+
+	return result;
+};
+
+module.exports = Config;
